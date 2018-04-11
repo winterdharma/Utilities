9eba481b40a1c84428a61eab8c76cf94fb555db8
Charlie Patton <cdpatton2009@gmail.com>
2018-04-11 15:39:02 -0500

    Code added from CBETAConcordanceMaker.

---
+++ IOFunctions/LoadFiles.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Utilities.IOFunctions
+{
+    public static class LoadFiles
+    {
+        /// <summary>
+        /// Searches supplied directory for files with "*.xml" extension and returns
+        /// a collection of XmlDocument objects. Invalid xml characters are escaped
+        /// before XmlDocument objects are created.
+        /// </summary>
+        /// <param name="dirPath"></param>
+        /// <returns></returns>
+        public static List<XmlDocument> XmlFiles(string dirPath)
+        {
+            if(Directory.Exists(dirPath))
+            {
+                var xmlDocs = new List<XmlDocument>();
+
+                var xmlFiles = Directory.GetFiles(dirPath + "\\", "*.xml").ToList();
+
+                ValidateXmlFiles(xmlFiles);
+
+                foreach (string file in xmlFiles)
+                {
+                    XmlDocument xml = new XmlDocument();
+                    xml.Load(file);
+                    xmlDocs.Add(xml);
+                }
+
+                return xmlDocs;
+            }
+            else
+            {
+                throw new ArgumentException("Directory path is not valid.");
+            }
+        }
+
+        private static void ValidateXmlFiles(List<string> xmlFilePaths)
+        {
+            foreach (string xmlFile in xmlFilePaths)
+            {
+                string str = File.ReadAllText(xmlFile);
+                str = str.Replace("&", "&amp;");
+                str = str.Replace("&amp;amp;", "&amp;");
+                str = str.Replace("'", "&apos;");
+                str = str.Replace("\"", "&quot;");
+                //str = str.Replace(">", "&gt;");
+                //str = str.Replace("<", "&lt;");
+                File.WriteAllText(xmlFile, str);
+            }
+        }
+
+        /// <summary>
+        /// Loads each line of a file into a collection and returns a List<string> object.
+        /// Throws an ArgumentException if the path supplied is not valid.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static List<string> TextFile(string filePath, Encoding encoding)
+        {
+            
+            if (Directory.Exists(filePath))
+            {
+                var textFile = new List<string>();
+
+                StreamReader input = new StreamReader(filePath, encoding);
+                string term = "";
+                while ((term = input.ReadLine()) != null)
+                {
+                    if (!term.Equals(""))
+                        textFile.Add(term);
+                }
+                input.Close();
+
+                return textFile;
+            }
+            else
+            {
+                throw new ArgumentException("File path is not valid.");
+            }
+        }
+    }
+}
---
+++ Unicode/UTF8.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Unicode
+{
+    public static class Utf8
+    {
+        /// <summary>
+        /// This method provides an accurate count of characters in an UTF-8 encoded string
+        /// taking into account any triple-byte Unicode characters encountered.
+        /// </summary>
+        /// <param name="utf8"></param>
+        /// <returns></returns>
+        public static int Length(string utf8)
+        {
+            int length = 0;
+            foreach (string cjk in AsCodePoints(utf8))
+            {
+                length++;
+            }
+            return length;
+        }
+
+
+        public static string Substring(string str, int startIndex, int length)
+        {
+            List<string> indexedCodePoints = new List<string>();
+            foreach (string cjk in AsCodePoints(str))
+            {
+                indexedCodePoints.Add(cjk);
+            }
+
+            var outputStr = new StringBuilder();
+            for (int i = startIndex; i < (startIndex + length); i++)
+            {
+                outputStr.Append(indexedCodePoints[i]);
+            }
+
+            return outputStr.ToString();
+        }
+
+
+        public static string Substring(string str, int startIndex)
+        {
+            List<string> indexedCodePoints = new List<string>();
+            foreach (string cjk in AsCodePoints(str))
+            {
+                indexedCodePoints.Add(cjk);
+            }
+
+            var sb = new StringBuilder();
+            for (int i = startIndex; i < indexedCodePoints.Count; i++)
+            {
+                sb.Append(indexedCodePoints[i]);
+            }
+
+            return sb.ToString();
+        }
+
+
+        private static IEnumerable<string> AsCodePoints(string str)
+        {
+            for (int i = 0; i < str.Length; ++i)
+            {
+                yield return char.ConvertFromUtf32(char.ConvertToUtf32(str, i));
+                if (char.IsHighSurrogate(str, i))
+                    i++;
+            }
+        }
+
+
+        public static string RemoveLeadingChars(string str, params char[] charsToRemove)
+        {
+            int index = 0;
+            bool charRemoved = false;
+            foreach (string character in AsCodePoints(str))
+            {
+                foreach (char chr in charsToRemove)
+                {
+                    if (character.Equals(chr.ToString()))
+                    {
+                        index++;
+                        charRemoved = true;
+                        break;
+                    }
+                }
+
+                if (!charRemoved)
+                    break;
+                charRemoved = false;
+            }
+            str = Substring(str, index);
+            return str;
+        }
+    }
+}
